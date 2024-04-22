f707aaed24a2c6abe68e20b43125103a324a9096
SijongKim93 <rlatlwhd456@gmail.com>
2024-04-23 01:04:31 +0900

    [Feat] 영화 목록 리스트 컬렉션 뷰 구현 , [Design] 로그인, 회원가입 오토레이아웃

--- BirdCinema/Controllers/MainViewController.swift
+++ BirdCinema/Controllers/MainViewController.swift
@@ -9,53 +9,103 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    let netWorking = NetworkingManager.shared
-    
+    let netWorkingManager = NetworkingManager.shared
+    var movieData: Welcome?
     
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var profileName: UILabel!
     @IBOutlet weak var profileIntro: UILabel!
-    
     @IBOutlet weak var mainSearchBar: UISearchBar!
+    @IBOutlet weak var mainPageImage: UIImageView!
+    @IBOutlet weak var collectionViewHeaderLabel: UILabel!
+    @IBOutlet weak var mainCollectionView: UICollectionView!
     
-    @IBOutlet weak var collectionViewLabel1: UILabel!
-    @IBOutlet weak var collectionViewLabel2: UILabel!
-    
-    @IBOutlet weak var collectionView1: UICollectionView!
-    @IBOutlet weak var collectionView2: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NetworkingManager.shared.fetchPopularMovies { result in
-                switch result {
-                case .success(let data):
-                    
-                    if let jsonString = String(data: data, encoding: .utf8) {
-                        print("Received data: \(jsonString)")
-                    } else {
-                        print("Failed to convert data to string.")
-                    }
-        
-                    do {
-                        let decoder = JSONDecoder()
-                        let decodedData = try decoder.decode(Welcome.self, from: data)
-                        print("Parsed data: \(decodedData)")
-                    } catch {
-                        print("Failed to decode data: \(error.localizedDescription)")
-                    }
-                    
-                case .failure(let error):
-                    print("Error fetching popular movies: \(error.localizedDescription)")
-                }
-            }
+        setupCollectionView()
+        fetchData()
+        configureUI()
     }
-
+    
+    func configureUI() {
+        collectionViewHeaderLabel.text = "박스오피스"
+    }
+    
+    func setupCollectionView() {
+        let flowLayout = createFlowLayout()
+        mainCollectionView.collectionViewLayout = flowLayout
+        mainCollectionView.delegate = self
+        mainCollectionView.dataSource = self
+        mainCollectionView.alwaysBounceHorizontal = true
+    }
+    
+    
+    func fetchData() {
+        netWorkingManager.fetchPopularMovies { [weak self] result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    self?.movieData = try decoder.decode(Welcome.self, from: data)
+                    DispatchQueue.main.async {
+                        self?.mainCollectionView.reloadData()
+                    }
+                } catch {
+                    print("\(error)")
+                }
+            case .failure(let error):
+                print("\(error)")
+            }
+        }
+    }
+    
+    
+    
+    func createFlowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.itemSize = CGSize(width: 200, height: 350)
+        return layout
+    }
     
     
     
     
     
-
+    
+    
+    
+    
+    
+    
+    @IBAction func mainPageController(_ sender: UIPageControl) {
+    }
+    
+    @IBAction func movieRevervationButtonTapped(_ sender: UIButton) {
+    }
 }
 
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieData?.results.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? MainCollectionViewCell else { fatalError("error")
+        }
+        
+        if let movieData = movieData {
+            let movie = movieData.results[indexPath.item]
+            cell.collectionMainLabel.text = movie.title
+            cell.collectionSubLabel.text = String(movie.voteCount)
+        }
+        
+        return cell
+        
+    }
+}
