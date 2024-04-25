05e29a2b97cd77a6a99fc5bfb4a99fd7e6ab4c66
정유진 <jeong-yujin@jeong-yujin-ui-MacBookAir.local>
2024-04-25 11:42:59 +0900

    [Fix] 씬델리게이트 화면

--- BirdCinema/SceneDelegate.swift
+++ BirdCinema/SceneDelegate.swift
@@ -11,6 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    /*
     // 지정한 storyboardName 이름을 갖는 Storyboard로 이동
     func move(to storyboardName: String) {
         let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
@@ -34,13 +35,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.move(to: "MovieReservation")
       }
     }
+    */
 
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-//        guard let _ = (scene as? UIWindowScene) else { return }
-//    }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
